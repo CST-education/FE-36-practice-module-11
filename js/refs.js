ee1ba189b5514837e4f634795e66a4a2069c11ff
AleksaHrevtsova <aleksandra.hrevtsova@gmail.com>
2021-09-11 00:06:14 +0300

    module-11 timer

---
+++ js/refs.js
@@ -0,0 +1,9 @@
+export default {
+  daysContent: document.getElementById('daysContent'),
+  hoursContent: document.getElementById('hoursContent'),
+  minsContent: document.getElementById('minsContent'),
+  secondsContent: document.getElementById('secondsContent'),
+  //
+  startBtn: document.querySelector('[data-action="start"]'),
+  stopBtn: document.querySelector('[data-action="stop"]'),
+}
